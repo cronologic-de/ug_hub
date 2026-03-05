50017c6b1118fb7bed947ee33d0aab89f71c4fa1
Max Kircher <max@cronologic.de>
2026-03-05 09:55:00 +0100

    fix top_of_page_button deprecation

--- source/conf.py
+++ source/conf.py
@@ -59,7 +59,7 @@ html_theme_options = {
         "color-highlight-on-target": cronolightorange,
         "sidebar-caption-space-above": "0",
     },
-    "top_of_page_button": None,
+    "top_of_page_buttons": [],
     "sidebar_hide_name": True,
 }
 
